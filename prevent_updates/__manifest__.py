d33671272ca9f04272b77fe95ed58c92c12591ac
Danny de Jong <ddejong@therp.nl>
2021-09-13 13:03:51 +0200

    [ADD] [no task] Initial commit.

---
+++ prevent_updates/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Prevent Accidental Updates",
+    'summary': "",
+    'description': """
+Prevents accidental updates by only allowing updates to be executed if an extra command line argument is given.
+""",
+    'author': "Danny de Jong",
+    'category': "???",
+    'version': '0.0.0',
+    'depends': [],
+    'data': [],
+}
---
+++ prevent_updates/main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+
+
+if '-u' in sys.argv:
+    if not 'CONFIRM_UPDATE' in os.environ or not os.environ['CONFIRM_UPDATE'] in ('1', 'true'):
+        print("The update has been cancelled.\n"
+"If you are 100% sure that your update does not reset existing data "
+"that has been manually changed and has noupdate set to 0, set "
+"environment variable CONFIRM_UPDATE=1 to confirm that that is really "
+"what you want to do.", file=sys.stderr)
+        sys.exit(1)
