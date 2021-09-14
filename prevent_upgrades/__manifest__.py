efa059878c40f8181c644c482c5aa6ae5f000310
Danny de Jong <ddejong@therp.nl>
2021-09-14 12:37:53 +0200

    [REF] [no task] Renamed prevent_updates to prevent_upgrades.

---
+++ prevent_upgrades/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Prevent Accidental Upgrades",
+    'summary': "Tries to prevent accidental upgrades done from the command line.",
+    'description': """
+Tries to prevent accidental upgrades done from the command line.
+""",
+    'author': "Danny de Jong",
+    'category': "Hidden",
+    'version': '0.0.0',
+    'depends': [],
+    'data': [],
+}
