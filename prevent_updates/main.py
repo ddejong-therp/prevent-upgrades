09f95d4ba1e85d70973cc033f818d8c6263c82a1
Danny de Jong <ddejong@therp.nl>
2021-09-14 12:34:08 +0200

    [FIX] [no task] Fixed environment variable name.

--- prevent_updates/main.py
+++ prevent_updates/main.py
@@ -4,7 +4,7 @@ import sys
 
 
 if '-u' in sys.argv:
-    if not 'CONFIRM_UPDATE' in os.environ or not os.environ['CONFIRM_UPDATE'] in ('1', 'true'):
+    if not 'CONFIRM_UPGRADE' in os.environ or not os.environ['CONFIRM_UPGRADE'] in ('1', 'true'):
         print("The update has been cancelled.\n"
 "If you are 100% sure that your update does not reset existing data "
 "that has been manually changed and has noupdate set to 0, set "
